e586988720d9700b527905982bdc428cec89db15
Vykstorm <victorruizgomezdev@gmail.com>
2019-03-05 20:50:42 +0100

    Added metric functions to evaluate the performance of the dl models

---
+++ models/metrics.py
@@ -0,0 +1,35 @@
+
+'''
+This script creates different ways to compute the score of a model given its
+label predictions and the truth labels
+'''
+
+import numpy as np
+from functools import partial
+
+def char_accuracy(y_pred, y_true):
+    '''
+    This function counts the number of correct character matches and divides it
+    by the total number of characters
+    '''
+    return (y_pred == y_true).flatten().mean()
+
+def word_accuracy(y_pred, y_true):
+    '''
+    This function counts the number of fully matched captchas and divides it by
+    the number of captchas
+    '''
+    return np.all(y_pred == y_true, axis=1).mean()
+
+
+def match_accuracy(n, y_pred, y_true):
+    '''
+    This function counts the number of partially matched captchas where there are at
+    least n occurences and divides it by the number of captchas
+    '''
+    return ((y_pred == y_true).sum(axis=1) >= n).mean()
+
+match1_accuracy = partial(match_accuracy, 1)
+match2_accuracy = partial(match_accuracy, 2)
+match3_accuracy = partial(match_accuracy, 3)
+match4_accuracy = partial(match_accuracy, 4)
