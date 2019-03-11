b9274ccdd25ba2d2ca215f06c2b406aa4fbe262c
Vykstorm <victorruizgomezdev@gmail.com>
2019-03-11 16:26:28 +0100

    Added parameter 'metrics'

--- models/metrics.py
+++ models/metrics.py
@@ -45,12 +45,20 @@ class FloydhubKerasCallback(BaseLogger):
     If it is used while your model is running on a floydhub server, training metrics
     will be plotted at real time under the 'Training metrics' panel.
     '''
-    def __init__(self, mode='epoch', stateful_metrics=None):
+    def __init__(self, mode='epoch', metrics=None, stateful_metrics=None):
         super().__init__(stateful_metrics)
 
         if mode not in ('epoch', 'batch'):
             raise ValueError('Mode parameter should be "epoch" or "batch"')
+
+        if metrics is not None and not isinstance(metrics, (list, tuple)):
+            raise ValueError('Metrics parameter should be a list of training metric names to track')
+
+        if  stateful_metrics is not None and not isinstance(metrics, (list, tuple)):
+            raise ValueError('Stateful metrics parameter should be a list of training metric names to track')
+
         self.mode = mode
+        self.metrics = frozenset(metrics)
         self.encoder = JSONEncoder()
 
     def report(self, metric, value, **kwargs):
@@ -61,11 +69,19 @@ class FloydhubKerasCallback(BaseLogger):
     def on_batch_end(self, batch, logs):
         if not self.mode == 'batch':
             return
-        for metric in frozenset(logs.keys()) - frozenset(['batch', 'size']):
+
+        metrics = frozenset(logs.keys()) - frozenset(['batch', 'size'])
+        if self.metrics:
+            metrics &= self.metrics
+        for metric in metrics:
             self.report(metric, logs[metric].item(), step=batch)
 
     def on_epoch_end(self, epoch, logs):
         if not self.mode == 'epoch':
             return
-        for metric in frozenset(logs.keys()):
+
+        metrics = frozenset(logs.keys())
+        if self.metrics:
+            metrics &= self.metrics
+        for metric in metrics:
             self.report(metric, logs[metric].item(), step=epoch)
