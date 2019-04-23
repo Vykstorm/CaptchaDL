bbea805c31ee99b50e0d6877b6d3ec654a918145
Vykstorm <victorruizgomezdev@gmail.com>
2019-04-23 13:29:00 +0200

    Now contours info is printed on stdout

--- models/contours.py
+++ models/contours.py
@@ -2,41 +2,54 @@
 import cv2 as cv
 import numpy as np
 from itertools import islice
+from functools import lru_cache
 
 class Contour:
     '''
     Represents a contour extracted from an image
     '''
-    def __init__(self, points, children=[]):
+    def __init__(self, points, img, children=[]):
+        '''
+        Initializes this instance
+        :param points: Must be a list the list of points that defines the contour
+        See OpenCV contours
+        :param img: Must be the image used to extract the contours from
+        :param children: A list of optional inner contours
+        '''
         self.points = points
+        self.source_img = img
         self.children = []
 
     @property
+    @lru_cache(maxsize=1)
     def bbox(self):
         '''
         Returns a non rotated rectangle that encapsulates this contour
         '''
-        left, top, width, height = cv.boundingRect(self.points)
-        return AABBRect(left, left + width, top, top + height)
+        return ContourBBox(self)
 
+    @property
     def bbox_width(self):
         '''
         Its the same as contour.bbox.width
         '''
         return self.bbox.width
 
+    @property
     def bbox_height(self):
         '''
         Its the same as contour.bbox.height
         '''
         return self.bbox.height
 
+    @property
     def bbox_area(self):
         '''
         Its the same as contour.bbox.area
         '''
         return self.bbox.area
 
+    @property
     def bbox_ratio(self):
         '''
         Its the same as contour.bbox.ratio
@@ -44,38 +57,40 @@ class Contour:
         return self.bbox.ratio
 
 
-    def bbox_pixels(self, img):
+    @property
+    def bbox_fill_ratio(self):
         '''
-        Its the same as contour.bbox.bbox_pixels()
+        Its the same as contour.bbox.fill_ratio
         '''
-        return self.bbox.bbox_pixels(img)
+        return self.bbox.fill_ratio
 
-
-    def bbox_fill_ratio(self, img):
-        '''
-        Its the same as contour.bbox.fill_ratio()
-        '''
-        return self.bbox.fill_ratio(img)
-
+    @property
     def area(self):
         '''
         Returns the area covered by this contour
         '''
         return cv.contourArea(self.points)
 
+    @property
     def extent(self):
         '''
         Returns the ratio between the areas of the contour and its bounding rectangle
         '''
         return self.area / self.bbox_area
 
+    @property
+    def perimeter(self):
+        '''
+        Returns the perimeter of this contour
+        '''
+        return cv.arcLength(self.points, True)
 
-    def extent(self):
+
+    def bbox_pixels(self, img):
         '''
-        Returns the extent of this contour. The ratio of the area defined by the
-        contour and the area of its bounding box
+        Its the same as contour.bbox.pixels()
         '''
-        return float(cv.contourArea(self.points)) / self.bbox_area
+        return self.bbox.bbox_pixels(img)
 
 
     def draw(self, img, show_children=False):
@@ -99,38 +114,53 @@ class Contour:
                 img = child.draw_bbox(img, show_children)
         return self.bbox.draw(img)
 
+
+    @property
+    @lru_cache(maxsize=1)
+    def properties(self):
+        '''
+        Returns a dictionary carrying information about this contour
+        '''
+        return {
+            'bbox_width': self.bbox_width,
+            'bbox_height': self.bbox_height,
+            'bbox_fill_ratio': self.bbox_fill_ratio,
+            'area': self.area,
+            'extent': self.extent,
+            'perimeter': self.perimeter
+        }
+
+
     def _draw(img, color=(0, 255, 0)):
         return cv.drawContours(img, self.points, -1, tuple(color))
 
 
 
-class AABBRect:
+class ContourBBox:
     '''
-    Represents an AABB Rectangle
+    Represents an AABB Rectangle bounding box of a contour
     '''
-    def __init__(self, left, right, top, bottom):
+    def __init__(self, contour):
         '''
-        :param left: Left side of the rectangle
-        :param right: Right side of the rectangle
-        :param top: Top side of the rectangle
-        :param bottom: Bottom side of the rectangle
+        :param contour: Is the contour that is surrounded by this rectangle box
         '''
-        self.left, self.right = left, right
-        self.top, self.bottom = top, bottom
+        self.inner_contour = contour
+        self.left, self.top, self.width, self.height = cv.boundingRect(contour.points)
+
 
     @property
-    def width(self):
+    def right(self):
         '''
-        Returns the width of the rectangle
+        Returns the right side of the rectangle
         '''
-        return self.right - self.left
+        return self.left + self.width
 
     @property
-    def height(self):
+    def bottom(self):
         '''
-        Returns the height of the rectangle
+        Returns the bottom of the rectangle
         '''
-        return self.bottom - self.top
+        return self.top + self.height
 
     @property
     def area(self):
@@ -147,6 +177,15 @@ class AABBRect:
         return self.width / self.height
 
 
+    @property
+    def fill_ratio(self):
+        '''
+        Returns the ratio of the number of non-black pixels (intensity greater than 0)
+        in the image used to extract the contours (inside the limits of the bounding box)
+        '''
+        return (self.pixels(self.inner_contour.source_img) > 0).flatten().mean()
+
+
     def pixels(self, img):
         '''
         Returns the pixels of the specified image that are inside this bounding
@@ -154,15 +193,6 @@ class AABBRect:
         '''
         return img[self.top:self.top+self.height, self.left:self.left+self.width]
 
-    def fill_ratio(self, img):
-        '''
-        Returns the ratio of the number of non-black pixels (intensity greater than 0)
-        in the specified image (inside the limits of the bounding box)
-        '''
-        return (self.pixels(img) > 0).flatten().mean()
-
-    def __str__(self):
-        return '({}, {}), {} x {}'.format(self.left, self.top, self.width, self.height)
 
     def draw(self, img, color=(0, 255, 0)):
         '''
@@ -172,6 +202,10 @@ class AABBRect:
                         (self.left,self.top), (self.left+self.width,self.top+self.height),
                         tuple(color), 1)
         return img
+
+    def __str__(self):
+        return '({}, {}), {} x {}'.format(self.left, self.top, self.width, self.height)
+
 
 def find_contours(img):
     '''
@@ -218,15 +252,16 @@ def find_contours(img):
     A = set([k for k in range(0, n) if len(contour_children[k]) == 0])
     B = set(range(0, n)) - A
     for k in A:
-        items[k] = Contour(contours[k])
+        items[k] = Contour(contours[k], processed)
 
     while len(B) > 0:
         C = B & set([contour_parent[k] for k in A])
         for j in C:
-            items[j] = Contour(contours[j], [items[k] for k in contour_children[j]])
+            items[j] = Contour(contours[j], processed, [items[k] for k in contour_children[j]])
         B -= C
 
     return [items[k] for k in range(0, n) if contour_parent[k] == -1]
+
 
 
 def draw_contours(img, contours, show_children=False):
@@ -239,10 +274,13 @@ def draw_contours(img, contours, show_children=False):
         img = contour.draw(img, show_children)
     return img
 
+
+
 def draw_bbox_contours(img, contours, show_children=False):
     for contour in contours:
         img = contour.draw_bbox(img, show_children)
     return img
+
 
 
 if __name__ == '__main__':
@@ -254,6 +292,9 @@ if __name__ == '__main__':
     from dataset import CaptchaDataset
     import matplotlib.pyplot as plt
     import pandas as pd
+    import operator
+    from itertools import product
+    from functools import reduce
 
     # Get a batch of samples of size n
     n = 9
@@ -269,6 +310,30 @@ if __name__ == '__main__':
         contours.append(find_contours(X_batch[k, :, :, 0]))
 
     # Show contour properties
+    contour_bbox_width, contour_bbox_height = [], []
+    contour_bbox_fill_ratio = []
+    contour_extent, contour_area = [], []
+    contour_id = []
+    for k in range(0, n):
+        for i in range(0, len(contours[k])):
+            contour = contours[k][i]
+            contour_id.append('Sample {}, index {}'.format(k, i))
+            contour_bbox_width.append(contour.bbox_width)
+            contour_bbox_height.append(contour.bbox_height)
+            contour_bbox_fill_ratio.append(contour.bbox_fill_ratio)
+            contour_extent.append(contour.extent)
+            contour_area.append(contour.area)
+
+    df = pd.DataFrame.from_dict({
+        'ids': contour_id,
+        'bbox_width': contour_bbox_width,
+        'bbox_height': contour_bbox_height,
+        'bbox_fill_ratio': contour_bbox_fill_ratio,
+        'extent': contour_extent,
+        'area': contour_area
+    })
+    print(df)
+
 
     # Show the images and draw the bounding boxes of the contours
     rows, cols = n // 2, 2
